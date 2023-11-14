5f000554e0daf606199959e0a4b6d7e7ef7d6adb
iKAN2025 <bellsparents@gmail.com>
2023-11-14 15:01:08 -0800

    yes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import threading
 
 # import "packages" from flask
 from flask import render_template  # import render_template from "public" flask libraries
-
+#yes
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
 from model.jokes import initJokes
